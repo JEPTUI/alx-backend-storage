1b00d9106447a525776ebc7bcacd5d202697ba97
JEPTUI <alicejeptui@gail.com>
2023-10-12 14:07:24 +0000

    Unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates a table users with id, email, name as attributes
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
