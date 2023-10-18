7730d9642585ebb03efaa252d84d1d8b60035e04
JEPTUI <alicejeptui@gail.com>
2023-10-18 13:46:31 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,7 +16,23 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument and returns a string"""
         key = str(uuid.uuid4())
-
         self._redis.set(key, data)
-
         return key
+
+    def get(self, key: str, fn: Callable[[
+        bytes], Union[str, int, float]] = [
+            lambda d:d]) -> Union[str, int, float, bytes]:
+        """take a key string argument and an optional Callable
+        that will convert the data back to the desired format"""
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """parametrize Cache.get with string"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """parametrize Cache.get with int"""
+        return self.get(key, fn=int)
